a2b658366102ba58a54f651e2a7741b0314a3854
Ikkir <lookatmeloony@gmail.com>
2018-08-31 16:30:15 -0500

    Game Controller dummy while discussing

---
+++ GTKJ2018/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * GMTK Jam 2018
+ * MintFox
+ * Sigrath
+ * Basinga
+ * Cuurian
+ * Ikkir
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+    void InitValues()
+    {
+        //insert whatever here later
+    }
+
+}
