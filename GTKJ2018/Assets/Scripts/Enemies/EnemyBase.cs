ec8116f12a6518eb082a8aae0506e8895665b97d
Watulion <rdo_cnt@hotmail.com>
2018-09-01 08:46:25 -0500

    Adding some enemy scripts. Up next making them functional and adding a prefab

---
+++ GTKJ2018/Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour {
+
+    //Variables
+    public bool canBeHurt = false;
+    public int health = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
