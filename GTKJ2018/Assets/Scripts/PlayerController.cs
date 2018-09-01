f96db10abc289caddcf6df9654dca844c2a1d482
JohnnysPC\Johnny <mintiestfox@gmail.com>
2018-08-31 23:59:03 -0500

    Added WIP parry bounce. Also an enemy prefab to bounce off of.

--- GTKJ2018/Assets/Scripts/PlayerController.cs
+++ GTKJ2018/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float walkSpeed_max = 5; // max walking speed
     public float walkSpeed_acceleration = 15; // how fast you move towards max walking speed
@@ -23,6 +24,13 @@ public class PlayerController : MonoBehaviour {
     private bool isGrounded = true;
 
     private bool jumpInput; // getting the input in the update, to use it in fixed update
+    private bool parryInput;
+
+    private bool isParrying;
+    public float parryRadius; //radius of where your parry effects
+    public float parryTime; //how long does the parry last?
+    public float parryBounceForce; //how high can you bounce off enemies?
+    public LayerMask parryLayers; // what can you parry?
 
     private Rigidbody2D rb;
     private Collider2D colli;
@@ -31,23 +39,28 @@ public class PlayerController : MonoBehaviour {
     public PhysicsMaterial2D physMat_air; // having no friction in the air keeps you from sliding on walls
 
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //grabbing your components
         colli = GetComponent<Collider2D>();
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetButtonDown("Jump")) // getting jump input
         {
             jumpInput = true;
         }
 
-	}
+        if (Input.GetButtonDown("Fire1"))
+        {
+            parryInput = true;
+        }
+
+    }
 
     private void FixedUpdate()
     {
@@ -58,19 +71,57 @@ public class PlayerController : MonoBehaviour {
         if (jumpInput && isGrounded) // check for ground before jumping
             StartCoroutine(Jump());
 
+        if (parryInput)
+            StartCoroutine(Parry());
+
+
         jumpInput = false; //clearing the input since it wasn't used
+        parryInput = false;
     }
 
+    IEnumerator Parry()
+    {
+        isParrying = true;
+        float startTime = Time.time;
+        while (Time.time - startTime < parryTime)
+        {
+            Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, parryRadius, parryLayers);
+            yield return new WaitForFixedUpdate();
+
+            if (hits.Length > 0)
+            {
+                if (!isGrounded)
+                {
+
+                    ParryBounce();
+                }
+                isParrying = false;
+                yield break;
+            }
+        }
+        isParrying = false;
+    }
+
+    void ParryBounce()
+    {
+        Vector2 newVel = rb.velocity;
+        newVel.y = 0;
+        rb.velocity = newVel;
+
+
+        rb.AddForce(Vector2.up * parryBounceForce, ForceMode2D.Impulse);
+
+    }
 
     void GroundCheck()
     {
 
         RaycastHit2D hit = Physics2D.CircleCast(transform.position, 0.1f, Vector2.down, 0.5f, groundLayers); // send a circle down to check for ground
 
-        if (hit)
+        if (hit && colli.IsTouchingLayers(groundLayers))
         {
             isGrounded = true;
-            colli.sharedMaterial = physMat_ground;
+            colli.sharedMaterial = physMat_ground; // keeps you from sliding on the floor
         }
         else
         {
@@ -103,6 +154,13 @@ public class PlayerController : MonoBehaviour {
 
     IEnumerator Jump()
     {
+
+        Vector2 newVel = rb.velocity;
+        if (rb.velocity.y < 0)
+            newVel.y = 0;
+
+        rb.velocity = newVel;
+
         rb.AddForce(Vector2.up * jump_forceInit, ForceMode2D.Impulse); //add the initial jump kick
 
 
