0962ae6d1baf1618bed1693c93642da0db1ca8ea
Ikkir <lookatmeloony@gmail.com>
2018-09-01 18:49:31 -0500

    Misc stuff to start bullet hitting

--- GTKJ2018/Assets/Scripts/Enemies/EnemyHitBox.cs
+++ GTKJ2018/Assets/Scripts/Enemies/EnemyHitBox.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class EnemyHitBox : MonoBehaviour
 {
-
+   
+    public bool destroyOnHit = false;
     //damage variable
     public int damageDealt = 0;
 
@@ -22,17 +23,62 @@ public class EnemyHitBox : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        //if it hits the player
-        if (other.GetComponent<PlayerController>() != null)
+        bool playerTest = true;
+        bool enemyTest = false;
+        if (this.transform.parent.GetComponent<Bullet>()) //gonna be ugly here but make it so
         {
-            Debug.Log("trigger enter " + gameObject.name);
-            //if the hitbox deals damage at all
-            if (damageDealt > 0)
+            Bullet bullet = this.transform.parent.GetComponent<Bullet>();
+            if (bullet.isReflected)
             {
-                //Put damage players function here
-                Debug.Log("trigger enter " + gameObject.name);
-                other.GetComponent<PlayerController>().TakeDamage(damageDealt);
+                //test for enemy hits
+                enemyTest = true;
+                playerTest = false;
+
+            }
+            else
+            {
+                enemyTest = false;
+                playerTest = true;
+                //test for player hits
             }
         }
+        //otherwise
+        if (playerTest)
+        {
+            if (other.GetComponent<PlayerController>() != null)
+            {
+                Debug.Log("trigger enter on player" + gameObject.name);
+                //if the hitbox deals damage at all
+                if (damageDealt > 0)
+                {
+                    //Put damage players function here
+                    
+                    other.GetComponent<PlayerController>().TakeDamage(damageDealt);
+                }
+                if (destroyOnHit)
+                {
+                    Destroy(this.transform.parent.gameObject); //hitbox always child direct to main object root
+                }
+            }
+        }
+        if (enemyTest)
+        {
+            if (other.GetComponent<EnemyBase>() != null)
+            {
+                Debug.Log("trigger enter on enemy" + gameObject.name);
+                //if the hitbox deals damage at all
+                if (damageDealt > 0)
+                {
+                    //Put damage players function here
+
+                    other.GetComponent<EnemyBase>().OnParried(this.transform);
+                }
+                if (destroyOnHit)
+                {
+                    Destroy(this.transform.parent.gameObject); //hitbox always child direct to main object root
+                }
+            }
+        }
+        
     }
 }
