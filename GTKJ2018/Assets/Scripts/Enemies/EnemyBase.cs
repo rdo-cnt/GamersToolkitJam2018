50ed4e17745730967aa61951df763774031885d2
Watulion <rdo_cnt@hotmail.com>
2018-09-01 08:58:56 -0500

    Added animation component support on enemy base. FIXED COMPILING ERROR (sorry)

--- GTKJ2018/Assets/Scripts/Enemies/EnemyBase.cs
+++ GTKJ2018/Assets/Scripts/Enemies/EnemyBase.cs
@@ -8,10 +8,17 @@ public class EnemyBase : MonoBehaviour {
     public bool canBeHurt = false;
     public int health = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //Animation Components
+    protected SpriteRenderer m_sr;
+    protected Animator m_anim;
+    AnimatorStateInfo currentState;
+    protected float playbackTime;
+
+    // Use this for initialization
+    void Start () {
+        m_anim = GetComponent<Animator>();
+        m_sr = GetComponent<SpriteRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- GTKJ2018/Assets/Scripts/Enemies/GroundedEnemyController.cs
+++ GTKJ2018/Assets/Scripts/Enemies/GroundedEnemyController.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class GroundedEnemyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //Get Collision Components
+    protected Rigidbody2D m_rb;
+    protected BoxCollider2D m_col;
+
+    //Animation Compnents
+    protected Animator m_anim;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        //getBoxCollider
+        m_col = GetComponent<BoxCollider2D>();
+
+        //getBoxCollider
+        m_rb = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
