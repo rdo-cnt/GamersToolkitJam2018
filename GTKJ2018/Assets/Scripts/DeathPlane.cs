b397877aa04afe9247f9b590b7b54e5cdb1e399a
Ikkir <lookatmeloony@gmail.com>
2018-09-02 12:08:52 -0500

    Work on lvl 3

--- GTKJ2018/Assets/Scripts/DeathPlane.cs
+++ GTKJ2018/Assets/Scripts/DeathPlane.cs
@@ -16,11 +16,15 @@ public class DeathPlane : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("triggering death on " + collision.gameObject.name);
-        if (collision.GetComponent<EnemyBase>() || collision.GetComponent<PlayerController>())
+        if (!collision.GetComponent<EnemyFlierController>())
         {
-            collision.gameObject.SendMessage("Die");
+            Debug.Log("triggering death on " + collision.gameObject.name);
+            if (collision.GetComponent<EnemyBase>() || collision.GetComponent<PlayerController>())
+            {
+                collision.gameObject.SendMessage("Die");
+            }
         }
+        
         
     }
 }
